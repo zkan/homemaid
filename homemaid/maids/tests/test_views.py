a4c8b0a806a3509405aed28185fe910ac10183f8
zkan <zkan.cs@gmail.com>
2020-07-18 16:25:35 +0700

    Use reverse :bear:

--- homemaid/maids/tests/test_views.py
+++ homemaid/maids/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestMaidListView(TestCase):
     def test_view_should_respond_200(self):
-        response = self.client.get('/maids/')
+        response = self.client.get(reverse('maid-list'))
         assert response.status_code == 200
--- homemaid/maids/urls.py
+++ homemaid/maids/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import MaidListView
 
+
 urlpatterns = [
     path('', MaidListView.as_view(), name='maid-list'),
 ]
