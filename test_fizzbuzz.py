b23e35104f614c97ded5d82437aa5be2a39d6b52
zkan <zkan.cs@gmail.com>
2020-07-18 10:32:10 +0700

    Finish fizz case :bear:

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,12 +2,17 @@ import unittest
 
 
 def fizzbuzz(number):
-    return 'Fizz'
+    if number % 3 == 0:
+        return 'Fizz'
 
 
 class TestFizzBuzz(unittest.TestCase):
     def test_input_3_should_get_fizz(self):
         result = fizzbuzz(3)
+        self.assertEqual(result, 'Fizz')
+    
+    def test_input_6_should_get_fizz(self):
+        result = fizzbuzz(6)
         self.assertEqual(result, 'Fizz')
 
 
