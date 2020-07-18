3838ebd1d56a320e2a6e12b7fdaf3b04e48e51aa
zkan <zkan.cs@gmail.com>
2020-07-18 16:52:03 +0700

    Add maid list view :bear:

--- homemaid/maids/tests/test_views.py
+++ homemaid/maids/tests/test_views.py
@@ -1,8 +1,36 @@
+from datetime import date
+
 from django.test import TestCase
 from django.urls import reverse
+
+from ..models import Maid
 
 
 class TestMaidListView(TestCase):
     def test_view_should_respond_200(self):
         response = self.client.get(reverse('maid-list'))
         assert response.status_code == 200
+
+    def test_view_should_display_maid_list(self):
+        # Given
+        Maid.objects.create(
+            name='BB',
+            birthdate=date(1998, 4, 29),
+            description='Super Maid of the Year',
+            certificate='Best Maid 2012',
+            salary=3000
+        )
+        Maid.objects.create(
+            name='CC',
+            birthdate=date(1998, 9, 30),
+            description='Ultra Maid of the Year',
+            certificate='Best Maid 2020',
+            salary=3200
+        )
+
+        # When
+        response = self.client.get(reverse('maid-list'))
+
+        # Then
+        assert '<li>BB</li>' in str(response.content)
+        assert '<li>CC</li>' in str(response.content)
--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -2,7 +2,23 @@
 from django.http import HttpResponse
 from django.views import View
 
+from .models import Maid
+
 
 class MaidListView(View):
     def get(self, request):
-        return HttpResponse()
+        # Maid.objects.all()
+        # m.name
+        # for m in Maid.objects.all():
+        # เราจะสามารถใช้ m.name ได้
+
+        # my_list = [1, 2, 3, 4, 5]
+        # for item in my_list:
+        #     print(item)
+
+        html = ''
+        maids = Maid.objects.all()
+        for maid in maids:
+            html += f'<li>{maid.name}</li>'
+
+        return HttpResponse(html)
