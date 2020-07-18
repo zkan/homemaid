66b16cde1c1b4a211204177d7c45b7f7a67afa8e
zkan <zkan.cs@gmail.com>
2020-07-18 14:25:34 +0700

    Add fiel field :bear:

--- homemaid/maids/models.py
+++ homemaid/maids/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Maid(models.Model):
     name = models.CharField(max_length=300)
+    profile_image = models.FileField()
     birthdate = models.DateField()
     description = models.TextField()
     certificate = models.TextField()
--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,5 +1,8 @@
+import os
 from datetime import date
+from unittest.mock import MagicMock
 
+from django.core.files import File
 from django.test import TestCase
 
 from .models import Maid
@@ -25,3 +28,25 @@ class TestMaid(TestCase):
         self.assertEqual(maid.description, 'Super Maid of the Year')
         self.assertEqual(maid.certificate, 'Best Maid 2012')
         self.assertEqual(maid.salary, 3000)
+
+    def test_model_should_have_image_field(self):
+        # Given
+        mock = MagicMock(spec=File)
+        mock.name = 'profile.png'
+
+        Maid.objects.create(
+            name='BB',
+            profile_image=mock,
+            birthdate=date(1998, 4, 29),
+            description='Super Maid of the Year',
+            certificate='Best Maid 2012',
+            salary=3000
+        )
+
+        # When
+        maid = Maid.objects.last()
+
+        # Then
+        self.assertEqual(maid.profile_image.name, 'profile.png')
+
+        os.remove('profile.png')
