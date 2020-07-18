7a19a7994f4b79dfc5100d5bcd31f99d8f5ca768
zkan <zkan.cs@gmail.com>
2020-07-18 10:25:19 +0700

    Add playground script :bear:

---
+++ playground.py
@@ -0,0 +1,35 @@
+a = 1
+print(a)
+
+b = 'hello'
+print(b)
+
+if a == 1 and (a == 1 or a == 1):
+    print('hi')
+    print('test')
+elif a == 2:
+    print('a is 2')
+else:
+    print('else!')
+
+if a is not None:
+    print('a is not None')
+
+if a:
+    print('a')
+
+if a is None:
+    print('a is None')
+
+def say():
+    print('hi')
+
+say()
+
+# prefer this
+def say1(number):
+    print(f'hi {number}')
+
+# not recommended
+def say2(number: int):
+    print('hi ' + str(number))
