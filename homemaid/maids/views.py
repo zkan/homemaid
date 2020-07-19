522cf5dd91b2edb2f477ff1c7c6e3d4a7c9f3fa5
zkan <zkan.cs@gmail.com>
2020-07-19 10:41:06 +0700

    Render context in template :bear:

--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
@@ -6,22 +6,13 @@ from .models import Maid
 
 
 class MaidListView(View):
+    template_name = 'maid_list.html'
+
     def get(self, request):
-        # Maid.objects.all()
-        # m.name
-        # for m in Maid.objects.all():
-        # เราจะสามารถใช้ m.name ได้
-
-        # my_list = [1, 2, 3, 4, 5]
-        # for item in my_list:
-        #     print(item)
-
-        html = ''
-        maids = Maid.objects.all()
-        for maid in maids:
-            html += f'<li>{maid.name}</li>'
-
-        return HttpResponse(html)
+        context = {
+            'maid_list': Maid.objects.all()
+        }
+        return render(request, self.template_name, context)
 
 
 def maid_another_list_view(request):
