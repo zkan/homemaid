01f3f69d268458f5c8662e585cf8c8221639b88d
zkan <zkan.cs@gmail.com>
2020-07-19 12:07:03 +0700

    Set fields not required and change settings regarding static files :bear:

--- homemaid/homemaid/settings/base.py
+++ homemaid/homemaid/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'maids',
     'django_extensions'
 ]
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+# STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
-# STATICFILES_DIRS = [
-#     os.path.join(SITE_ROOT, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(SITE_ROOT, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
--- homemaid/maids/models.py
+++ homemaid/maids/models.py
@@ -5,8 +5,8 @@ from django_extensions.db.models import TimeStampedModel
 
 class Maid(TimeStampedModel):
     name = models.CharField(max_length=300)
-    profile_image = models.FileField()
-    birthdate = models.DateField()
-    description = models.TextField()
-    certificate = models.TextField()
-    salary = models.IntegerField()
+    profile_image = models.FileField(blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    certificate = models.TextField(blank=True, null=True)
+    salary = models.IntegerField(blank=True, null=True)
