b05dca00d270582ddaf0a745e121112a33f21251
zkan <zkan.cs@gmail.com>
2020-07-19 11:00:06 +0700

    Serve media files :bear:

--- homemaid/homemaid/settings/base.py
+++ homemaid/homemaid/settings/base.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
--- homemaid/homemaid/urls.py
+++ homemaid/homemaid/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
 
 from maids.views import MaidListView
@@ -23,3 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('maids/', include('maids.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
