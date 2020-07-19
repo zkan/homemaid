754e642d6ab864731cc3d4f1b32f406be30541ac
zkan <zkan.cs@gmail.com>
2020-07-19 15:52:09 +0700

    Register DRF to Django :bear:

--- homemaid/homemaid/settings/base.py
+++ homemaid/homemaid/settings/base.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'maids',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
